f803b8d237bb751e072938e507dbcedc4dcb21e4
BoraAbramov <borya812@gmail.com>
2026-02-25 21:33:44 +0200

    update

--- def prac.py
+++ def prac.py
@@ -92,13 +92,55 @@ def display_account_menu():
     print('3. Transfer')
     print('4. Exit')
 
-def get_account_menu_choice()
+def get_account_menu_choice():
     choice = int(input('Choose 1-4:'))
     return choice
 
+def get_deposit_amount():
+    deposit_amount = None
+    while deposit_amount < 1:
+        deposit_amount = int(input('whats your deposit amount?'))
+    return deposit_amount
 
 
+def deposit(user_account_number, balance, deposit_amount):
+    # account      0     1        2     3   4    5
+    # balance = [300, 4000, 100_000, 5000, 0, 900]
+    if (deposit_amount) >= balance[user_account_number]:
+        balance[user_account_number] -= (deposit_amount)
+        return balance[user_account_number]
+    else:
+        print('you dont have enough money')
 
+def get_withdraw_amount():
+    withdraw_amount = None
+    while withdraw_amount < 1:
+        withdraw_amount = int(input('whats your deposit amount?'))
+    return withdraw_amount
+
+def withdraw(user_account_number, balance, withdraw_amount):
+    if (withdraw_amount) >= balance[user_account_number]:
+        balance[user_account_number] -= (withdraw_amount)
+        return balance[user_account_number]
+    else:
+        print('you dont have enough money')
+
+def get_transfer_amount():
+    transfer_amount = None
+    while transfer_amount < 1:
+        transfer_amount = int(input('whats your transfer amount?'))
+
+def get_transfer_to_account(balance):
+    transfer_to_account = int(input('for what account to transfer?'))
+    return transfer_to_account
+
+def enough_to_transfer(user_account_number, balance, transfer_amount):
+    if balance[user_account_number] < transfer_amount:
+        return False
+
+def transfer(user_account_number, balance, trasfer_to_account):
+    balance[user_account_number] -= (withdraw_amount)
+    return balance[user_account_number]
 
 
 while True:
@@ -124,17 +166,24 @@ while True:
         display_account_menu()
         choice = get_account_menu_choice()
         match choice:
-            case 1: deposit_amount = get_deposit_amount()
-                deposit(user_account_number, balance, deposit_amount)
+            case 1:
+                    deposit_amount = get_deposit_amount()
+                    deposit(user_account_number, balance, deposit_amount)
                     continue
-            case 2: withdraw_amount = get_withdraw_amount()
-                withdraw(user_account_number, balance, withdraw_amount)
-            case 3: transfer_amount = get_transfer_amount()
-                trasfer_to_account = get_transfer_to_account(balance)
-                if check_if_valid(trasfer_to_account, accounts) == False:
-                    print('account not valid...')
+            case 2:
+                    withdraw_amount = get_withdraw_amount()
+                    withdraw(user_account_number, balance, withdraw_amount)
                     continue
-                transfer(user_account_number, balance, trasfer_to_account)
+            case 3:
+                    transfer_amount = get_transfer_amount()
+                    if enough_to_transfer(user_account_number, balance, transfer_amount) == False:
+                        print('transfer failed, go to work')
+                        continue
+                    trasfer_to_account = get_transfer_to_account(balance)
+                    if check_if_valid(trasfer_to_account, accounts) == False:
+                        print('account not valid...')
+                        continue
+                    transfer(user_account_number, balance, trasfer_to_account)
             case 4: break
             case _: print('invalid choice')
 
